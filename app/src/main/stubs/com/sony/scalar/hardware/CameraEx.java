ed97c4c6f7a33fa56464205b5cfdef05adca2eb7
James <jbuch84@gmail.com>
2026-03-01 21:16:15 -0500

    cant even

--- app/src/main/stubs/com/sony/scalar/hardware/CameraEx.java
+++
@@ -1,8 +0,0 @@
-package com.sony.scalar.hardware;
-import android.hardware.Camera;
-public class CameraEx {
-    public static CameraEx open(int id, Context context) { return null; }
-    public Camera getNormalCamera() { return null; }
-    public void release() {}
-    public interface Context {}
-}
--- app/src/main/stubs/com/sony/scalar/sysutil/ScalarInput.java
+++
@@ -1,4 +0,0 @@
-package com.sony.scalar.sysutil;
-public class ScalarInput {
-    public static final int ISV_KEY_S2 = 0;
-}
