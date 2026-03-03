98c14adde1ef888b9979f0d1c71912d53cfe5009
James <jbuch84@gmail.com>
2026-03-02 21:44:02 -0500

    4

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,5 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_github_ma1co_pmcademo_app_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "C++ Engine Online";
-    return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF("C++ Engine Online");
 }
